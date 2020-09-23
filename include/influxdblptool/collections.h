90cf3a9f461fd124d255e30465c574b229e0200e
Patrick Vollebregt <patrick@crosscode.nl>
2020-09-23 20:21:41 +0200

    Moved serializable_map to intern namespace

--- include/influxdblptool/collections.h
+++ include/influxdblptool/collections.h
@@ -8,25 +8,30 @@
 
 namespace influxdblptool {
 
-    template<typename TKey, typename TValue>
-    class serializable_map : public std::map<TKey, TValue>, public abstractions::serializable {
-        public:
-        using std::map<TKey, TValue>::map;
-        void serialize(std::ostream &s) const override {
-            auto serialize_pair = [&s,first=true](auto item) mutable {
-                if (first) {
-                    first = false;
-                } else {
-                    s << ",";
-                }
-                s << item.first << "=" << item.second;
-            };
-            std::for_each(begin(*this),end(*this),serialize_pair);
-        }
-    };
+    namespace intern {
 
-    using tags = serializable_map<tag_key, tag_value>;
-    using fields = serializable_map<field_key, field_value>;
+        template<typename TKey, typename TValue>
+        class serializable_map : public std::map<TKey, TValue>, public abstractions::serializable {
+        public:
+            using std::map<TKey, TValue>::map;
+
+            void serialize(std::ostream &s) const override {
+                auto serialize_pair = [&s, first = true](auto item) mutable {
+                    if (first) {
+                        first = false;
+                    } else {
+                        s << ",";
+                    }
+                    s << item.first << "=" << item.second;
+                };
+                std::for_each(begin(*this), end(*this), serialize_pair);
+            }
+        };
+
+    }
+
+    using tags = intern::serializable_map<tag_key, tag_value>;
+    using fields = intern::serializable_map<field_key, field_value>;
     using tag = tags::value_type;
     using field = fields::value_type;
 }
