e5f500295453661109789f3e60c2b947cc5e02cd
Patrick Vollebregt <patrick@crosscode.nl>
2020-09-25 10:46:52 +0200

    Added counters to escapers and optimize escaping to string instead of stringstream

--- src/influxdblptool/escapers.cpp
+++ src/influxdblptool/escapers.cpp
@@ -1,40 +1,76 @@
 #include "influxdblptool/escapers.h"
+#include <numeric>
 
 namespace influxdblptool::escapers {
 
+
+    template <char... Ch>
+    std::size_t escape_count(std::string_view input) {
+        auto char_count = [](std::size_t& in, const char& inputChar) {
+            auto needsEscape = [](const char& matchChar) {
+                return (...||(Ch==matchChar));
+            };
+            return in + ((needsEscape(inputChar)?std::size_t{2}:std::size_t{1}));
+        };
+
+        return std::accumulate(begin(input), end(input),std::size_t{0},char_count);
+    }
+
     template <char... Ch>
     std::string escape(std::string_view input) {
-        std::stringstream ss;
-        std::for_each(begin(input), end(input), [&ss](const char& inputChar){
+        std::string result;
+        result.reserve(escape_count<Ch...>(input));
+        auto escape_char = [&result](const char& inputChar){
             auto needsEscape = [](const char& matchChar) {
                 return (...||(Ch==matchChar));
             };
             if (needsEscape(inputChar)) {
-                ss << '\\';
+                result += '\\';
             }
-            ss << inputChar;
-        });
-        return ss.str();
+            result += inputChar;
+        };
+        std::for_each(begin(input), end(input),escape_char);
+        return result;
     }
 
     std::string escape_measurement(std::string_view s) {
         return escape<',', ' '>(s);
     }
 
+    std::size_t escape_measurement_count(std::string_view s) {
+        return escape_count<',', ' '>(s);
+    }
+
     std::string escape_tag_key(std::string_view s) {
         return escape<',', '=', ' '>(s);
+    }
+
+    std::size_t escape_tag_key_count(std::string_view s) {
+        return escape_count<',', '=', ' '>(s);
     }
 
     std::string escape_tag_value(std::string_view s) {
         return escape<',', '=', ' '>(s);
     }
 
+    std::size_t escape_tag_value_count(std::string_view s) {
+        return escape_count<',', '=', ' '>(s);
+    }
+
     std::string escape_field_key(std::string_view s) {
         return escape<',', '=', ' '>(s);
     }
 
+    std::size_t escape_field_key_count(std::string_view s) {
+        return escape_count<',', '=', ' '>(s);
+    }
+
     std::string escape_field_string_value(std::string_view s) {
         return escape<'\"', '\\'>(s);
+    }
+
+    std::size_t escape_field_string_value_count(std::string_view s) {
+        return escape_count<'\"', '\\'>(s);
     }
 
 }
--- tests/escapers_tests.cpp
+++ tests/escapers_tests.cpp
@@ -19,9 +19,19 @@ TEST_SUITE("escapers") {
         CHECK_EQ("\\,\\ \"\\=abc"s,escape_measurement(", \"\\=abc"s));
     }
 
+    TEST_CASE("escape_measurement_count counts: 8 (original length) + 2 (escaped chars) = 10")
+    {
+        CHECK_EQ(10,escape_measurement_count(", \"\\=abc"s));
+    }
+
     TEST_CASE("escape_tag_key escapes: Comma, Equals Sign, Space")
     {
         CHECK_EQ("\\,\\ \"\\\\=abc"s,escape_tag_key(", \"\\=abc"s));
+    }
+
+    TEST_CASE("escape_tag_key escapes: 8 (original length) + 3 (escaped chars) = 11")
+    {
+        CHECK_EQ(11,escape_tag_key_count(", \"\\=abc"s));
     }
 
     TEST_CASE("escape_tag_value escapes: Comma, Equals Sign, Space")
@@ -29,14 +39,29 @@ TEST_SUITE("escapers") {
         CHECK_EQ("\\,\\ \"\\\\=abc"s,escape_tag_value(", \"\\=abc"s));
     }
 
+    TEST_CASE("escape_tag_value_count escapes: 8 (original length) + 3 (escaped chars) = 11")
+    {
+        CHECK_EQ(11,escape_tag_value_count(", \"\\=abc"s));
+    }
+
     TEST_CASE("escape_field_key escapes: Comma, Equals Sign, Space")
     {
         CHECK_EQ("\\,\\ \"\\\\=abc"s,escape_field_key(", \"\\=abc"s));
     }
 
-    TEST_CASE("escape_field_value escapes: Double quote, Backslash")
+    TEST_CASE("escape_field_key_count escapes: 8 (original length) + 3 (escaped chars) = 11")
+    {
+        CHECK_EQ(11,escape_field_key_count(", \"\\=abc"s));
+    }
+
+    TEST_CASE("escape_field_string_value escapes: Double quote, Backslash")
     {
         CHECK_EQ(", \\\"\\\\=abc"s, escape_field_string_value(", \"\\=abc"s));
+    }
+
+    TEST_CASE("escape_field_string_value_count counts: 8 (original length) + 2 (escaped chars) = 10")
+    {
+        CHECK_EQ(10,escape_field_string_value_count(", \"\\=abc"s));
     }
 }
 
