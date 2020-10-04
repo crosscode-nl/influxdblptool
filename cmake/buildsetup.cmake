8a27b163ea0316aa3f6e23fd7f25327e69f4856a
Patrick Vollebregt <patrick@crosscode.nl>
2020-10-04 14:38:57 +0200

    Made code coverage optional

--- cmake/buildsetup.cmake
+++ cmake/buildsetup.cmake
@@ -1,5 +1,11 @@
+option(INFLUXDBLPTOOL_ENABLE_CODE_COVERAGE "Set this to ON to build unit tests")
+
 add_compile_options("$<$<CONFIG:RELEASE>:-W;-Wall;-Werror;-Wshadow;-Wextra;-Wpedantic;-pedantic-errors>")
-add_compile_options("$<$<CONFIG:DEBUG>:-W;-Wall;-Werror;-Wshadow;-Wextra;-Wpedantic;-pedantic-errors;-fprofile-arcs;-ftest-coverage>")
-add_link_options("$<$<CONFIG:DEBUG>:-lgcov;-coverage>")
+if (INFLUXDBLPTOOL_ENABLE_CODE_COVERAGE)
+    add_compile_options("$<$<CONFIG:DEBUG>:-W;-Wall;-Werror;-Wshadow;-Wextra;-Wpedantic;-pedantic-errors;-fprofile-arcs;-ftest-coverage>")
+    add_link_options("$<$<CONFIG:DEBUG>:-lgcov;-coverage>")
+else()
+    add_compile_options("$<$<CONFIG:DEBUG>:-W;-Wall;-Werror;-Wshadow;-Wextra;-Wpedantic;-pedantic-errors;>")
+endif()
 
 set(CMAKE_CXX_STANDARD 17)
