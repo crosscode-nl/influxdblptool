412d34df9300571ac569e943dbc466f309783c9c
Patrick Vollebregt <patrick@crosscode.nl>
2020-10-05 15:28:09 +0200

    Reduced CMake version to 3.8.2

--- benchmarks/cmake/googlebenchmark.cmake
+++ benchmarks/cmake/googlebenchmark.cmake
@@ -1,9 +1,9 @@
 # Download and unpack googletest at configure time
-configure_file(cmake/googlebenchmark.cmake.in googlebenchmark-download/CMakeLists.txt)
-
 SET(BENCHMARK_ENABLE_GTEST_TESTS OFF)
-
+SET(BENCHMARK_ENABLE_TESTING OFF)
 SET(BENCHMARK_DOWNLOAD_DEPENDENCIES ON)
+
+configure_file(cmake/googlebenchmark.cmake.in googlebenchmark-download/CMakeLists.txt)
 
 execute_process(COMMAND "${CMAKE_COMMAND}" -G "${CMAKE_GENERATOR}" .
         WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/googlebenchmark-download"
